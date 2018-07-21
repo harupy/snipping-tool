45d15788f6ceac10efb36863af9038a1795aceea
Tal Sheldon <talsheldon@hotmail.com>
2018-07-21 18:32:07 +0300

    merged new window class and menu class, now each snip will  open a new menu window as desired

--- SnippingMenu.py
+++ SnippingMenu.py
@@ -1,18 +1,19 @@
 import sys
-from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QAction, QWidget, QLabel, QMainWindow, QVBoxLayout, QApplication
+from PyQt5.QtGui import QPixmap, QImage
+import SnippingTool
 
-from SnippingTool import SnippingWidget
 
+class Menu(QMainWindow):
 
-class Menu(QtWidgets.QMainWindow):
-
-    def __init__(self):
+    # The first initialization will always be without a snip, so put None in both arguments.
+    def __init__(self, numpy_image=None, snip_number=None):
         super().__init__()
-        newAct = QtWidgets.QAction('New', self)
+        newAct = QAction('New', self)
         newAct.setShortcut('Ctrl+N')
         newAct.triggered.connect(self.new_image_window)
 
-        exitAct = QtWidgets.QAction('Exit', self)
+        exitAct = QAction('Exit', self)
         exitAct.setShortcut('Ctrl+Q')
         exitAct.setStatusTip('Exit application')
         exitAct.triggered.connect(self.close)
@@ -22,13 +23,30 @@ class Menu(QtWidgets.QMainWindow):
         toolbar.addAction(newAct)
         toolbar.addAction(exitAct)
 
-        self.snippingTool = SnippingWidget()
+        self.snippingTool = SnippingTool.SnippingWidget()
 
         self.setGeometry(300, 300, 350, 250)
-        self.setWindowTitle('Main window')
+        self.setWindowTitle('Snipping Tool')
+
+        # From the second initialization, both arguments will be valid
+        if numpy_image is not None and snip_number is not None:
+            self.setWindowTitle("Snip #{0}".format(snip_number))
+            self.central_widget = QWidget()
+            self.setCentralWidget(self.central_widget)
+
+            lay = QVBoxLayout(self.central_widget)
+            label = QLabel(self)
+            pixmap = self.convert_numpy_img_to_qpixmap(numpy_image)
+            label.setPixmap(pixmap)
+            self.resize(pixmap.width(), pixmap.height())
+            lay.addWidget(label)
+
         self.show()
 
+    # snippingTool.start() will open a new window, so if this is the first snip, close the first window.
     def new_image_window(self):
+        if not self.snippingTool.snips:
+            self.close()
         self.snippingTool.start()
 
     # quitting in the main menu will close all windows as well.
@@ -36,10 +54,15 @@ class Menu(QtWidgets.QMainWindow):
         for snip in self.snippingTool.snips:
             snip.close()
 
+    @staticmethod
+    def convert_numpy_img_to_qpixmap(np_img):
+        height, width, channel = np_img.shape
+        bytesPerLine = 3 * width
+        return QPixmap(QImage(np_img.data, width, height, bytesPerLine, QImage.Format_RGB888).rgbSwapped())
 
 
 if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     ex = Menu()
 
     sys.exit(app.exec_())
--- SnippingTool.py
+++ SnippingTool.py
@@ -1,14 +1,15 @@
-import sys
-from PyQt5 import QtWidgets, QtCore, QtGui
 import tkinter as tk
-from PIL import ImageGrab
 import numpy as np
 import cv2
-
-from ImageNewWindow import NewImage
+from PIL import ImageGrab
+from PyQt5 import QtWidgets, QtCore, QtGui
+import SnippingMenu
 
 
 class SnippingWidget(QtWidgets.QWidget):
+    snips = []
+    num_snip = 0
+
     def __init__(self):
         super().__init__()
         root = tk.Tk()
@@ -18,29 +19,24 @@ class SnippingWidget(QtWidgets.QWidget):
         self.setWindowTitle(' ')
         self.begin = QtCore.QPoint()
         self.end = QtCore.QPoint()
-        self.num_snip = 0
         self.is_snipping = False
-        self.snips = []
 
     def start(self):
         self.is_snipping = True
         self.setWindowOpacity(0.3)
-        QtWidgets.QApplication.setOverrideCursor(
-            QtGui.QCursor(QtCore.Qt.CrossCursor)
-        )
+        QtWidgets.QApplication.setOverrideCursor(QtGui.QCursor(QtCore.Qt.CrossCursor))
         self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
         print('Capture the screen...')
         print('Press q if you want to quit...')
         self.show()
 
     def paintEvent(self, event):
-        print("in draw event")
         if self.is_snipping:
             brush_color = (128, 128, 255, 100)
             lw = 3
             opacity = 0.3
         else:
-            # reset points, so the triangle won't
+            # reset points, so the rectangle won't show up again.
             self.begin = QtCore.QPoint()
             self.end = QtCore.QPoint()
             brush_color = (0, 0, 0, 0)
@@ -61,7 +57,6 @@ class SnippingWidget(QtWidgets.QWidget):
         event.accept()
 
     def mousePressEvent(self, event):
-        #print("mouse press event!")
         self.begin = event.pos()
         self.end = self.begin
         self.update()
@@ -71,10 +66,7 @@ class SnippingWidget(QtWidgets.QWidget):
         self.update()
 
     def mouseReleaseEvent(self, event):
-        # self.close()
-
-        print("mouse release event!")
-        self.num_snip += 1
+        SnippingWidget.num_snip += 1
         self.is_snipping = False
         QtWidgets.QApplication.restoreOverrideCursor()
         x1 = min(self.begin.x(), self.end.x())
@@ -85,18 +77,19 @@ class SnippingWidget(QtWidgets.QWidget):
         self.repaint()
         QtWidgets.QApplication.processEvents()
         img = ImageGrab.grab(bbox=(x1, y1, x2, y2))
-        self.repaint()
         QtWidgets.QApplication.processEvents()
-        img_name = 'snip{}.png'.format(self.num_snip)
+        img_name = 'snip{}.png'.format(SnippingWidget.num_snip)
         img.save(img_name)
         print(img_name, 'saved')
         img = cv2.cvtColor(np.array(img), cv2.COLOR_BGR2RGB)
-        self.snips.append(NewImage(img, self.num_snip))
+
+        # add to the snips list the object that opens a window of the image
+        SnippingWidget.snips.append(SnippingMenu.Menu(img, SnippingWidget.num_snip))
 
 
-if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    window = SnippingWidget()
-    window.show()
-    app.aboutToQuit.connect(app.deleteLater)
-    sys.exit(app.exec_())
+# if __name__ == '__main__':
+#     app = QtWidgets.QApplication(sys.argv)
+#     window = SnippingWidget()
+#     window.show()
+#     app.aboutToQuit.connect(app.deleteLater)
+#     sys.exit(app.exec_())
